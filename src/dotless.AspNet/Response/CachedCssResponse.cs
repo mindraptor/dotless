64507f6caa8455faa19febbd1c63441333f26c81
mindraptor <mraptor@outlook.com>
2012-09-16 13:48:26 +0200

    fixed merge error

--- src/dotless.AspNet/Response/CachedCssResponse.cs
+++ src/dotless.AspNet/Response/CachedCssResponse.cs
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-﻿namespace dotless.Core.Response
-{
-    using System;
-    using System.Web;
-    using Abstractions;
-
-    public class CachedCssResponse : CssResponse
-    {
-        private const int CacheAgeMinutes = 10080; //7 days
-
-        public CachedCssResponse(IHttp http, bool isCompressionHandledByResponse) : base(http, isCompressionHandledByResponse)
-        {
-        }
-
-        public override void WriteCss(string css)
-        {
-            var response = Http.Context.Response;
-
-            response.Cache.SetCacheability(HttpCacheability.Public);
-           
-            response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(CacheAgeMinutes));
-            response.Cache.SetETagFromFileDependencies();
-            response.Cache.SetLastModifiedFromFileDependencies();
-
-            //response.Cache.SetOmitVaryStar(true);
-
-            //WriteCss is called multiple times when there is an error in the less file and the AspResponseLogger is being used.
-            //SetVaryByCustom("Accept-Encoding") can only be set once, ignore this error when it happens.
-            try
-            {
-                response.Cache.SetVaryByCustom("Accept-Encoding");
-            }
-            catch { }
-
-            base.WriteCss(css);
-        }
-    }
-=======
 ﻿namespace dotless.Core.Response
 {
     using System;
@@ -67,5 +28,4 @@
             base.WriteHeaders();
         }
     }
->>>>>>> 3b1aec52bc6cd5a87355492613de044d83262444
 }
